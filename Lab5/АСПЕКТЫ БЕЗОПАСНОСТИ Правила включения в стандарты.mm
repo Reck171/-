5ec14735466e85c8029df14838da02e04630ac58
Reck171 <90756096+Reck171@users.noreply.github.com>
2023-05-18 22:00:43 +0300

    Add files via upload

---
+++ Lab5/АСПЕКТЫ БЕЗОПАСНОСТИ Правила включения в стандарты.mm
@@ -0,0 +1,721 @@
+<map version="freeplane 1.11.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="АСПЕКТЫ БЕЗОПАСНОСТИ&#xa;Правила включения в стандарты" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1684434924766"><hook NAME="MapStyle" background="#2e3440" zoom="0.207">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/dark_nord_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_671184412" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#eceff4" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#88c0d0" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_671184412" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" BORDER_WIDTH="1.9 px">
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ebcb8b">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#484747">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#5e81ac"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_779275544" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_779275544"/>
+<font SIZE="14"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#eceff4">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="1 Область применения" POSITION="bottom_or_right" ID="ID_1163273714" CREATED="1684434927308" MODIFIED="1684434939091"/>
+<node TEXT="3 Определения" POSITION="bottom_or_right" ID="ID_577887739" CREATED="1684434961710" MODIFIED="1684434975497">
+<node ID="ID_1073901866" CREATED="1684435024020" MODIFIED="1684435024020"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    3.1 безопасность: Отсутствие недопустимого риска
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1067554798" CREATED="1684435047070" MODIFIED="1684435047070"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    3.2 риск: Сочетание вероятности нанесения ущерба и тяжести этого ущерба.
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_66861610" CREATED="1684435061857" MODIFIED="1684435061857"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    3.3 ущерб: Нанесение физического повреждения или другого вреда здоровью людей, или вреда имуществу или окружающей среде.
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_473548157" CREATED="1684435069975" MODIFIED="1684435069975"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    3.4 вызывающее ущерб событие: Событие, при котором опасная ситуация приводит к ущербу.
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_159518882" CREATED="1684435077736" MODIFIED="1684435077736"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    3.5 опасность: Потенциальный источник возникновения ущерба.
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_492639874" CREATED="1684435086799" MODIFIED="1684435086799"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    3.6 опасная ситуация: Обстоятельства, в которых люди, имущество или окружающая среда подвергаются опасности.
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_418292673" CREATED="1684435094076" MODIFIED="1684435094076"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    3.7 допустимый риск: Риск, который в данной ситуации считают приемлемым при существующих общественных ценностях.
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1822566540" CREATED="1684435101400" MODIFIED="1684435101400"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    3.8 защитная мера: Мера, используемая для уменьшения риска.
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1019149890" CREATED="1684435110582" MODIFIED="1684435110582"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    3.9 остаточный риск: Риск, остающийся после предпринятых защитных мер.
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_239844725" CREATED="1684435120195" MODIFIED="1684435120195"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    3.10 анализ риска: Систематическое использование информации для выявления опасности и количественной оценки риска.
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_10842905" CREATED="1684435129250" MODIFIED="1684435129250"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    3.11 оценивание риска: Основанная на результатах анализа риска процедура проверки, устанавливающая, не превышен ли допустимый риск.
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_741691645" CREATED="1684435136188" MODIFIED="1684435136188"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    3.12 оценка риска: Общий процесс анализа риска и оценивания риска.
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1409096475" CREATED="1684435160275" MODIFIED="1684435160275"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    3.13 предназначенное использование: Использование продукции, процесса или услуги в соответствии с информацией, представленной поставщиком.
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_207974954" CREATED="1684435168362" MODIFIED="1684435168362"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    3.14 возможное предсказуемое неправильное использование: Использование продукции, процесса или услуги не предназначенным поставщиком образом, а вследствие предсказуемого поведения человека.
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node POSITION="top_or_left" ID="ID_1020036293" CREATED="1684435194975" MODIFIED="1684435194975"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    2 Нормативные ссылки
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node POSITION="top_or_left" ID="ID_503385441" CREATED="1684435214863" MODIFIED="1684435214863"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    4 Использование слов «безопасность» и «безопасный»
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node POSITION="bottom_or_right" ID="ID_47218636" CREATED="1684435233288" MODIFIED="1684435233288"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    5 Концепция безопасности
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_423343778" CREATED="1684435253050" MODIFIED="1684435253050"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    5.1 Аспекты безопасности необходимо учитывать в деятельности по стандартизации во многих областях для большинства видов продукции, процессов и услуг.
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_941668756" CREATED="1684435261748" MODIFIED="1684435261748"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    5.2 Безопасность достигают путем снижения уровня риска до допустимого, определенного в настоящем стандарте как допустимый риск. Допустимый риск представляет собой оптимальный баланс между безопасностью и требованиями, которым должны удовлетворять продукция, процесс или услуга, а также такими факторами, как выгодность для пользователя, эффективность затрат, обычаи и др.
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_306948941" CREATED="1684435268180" MODIFIED="1684435268180"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    5.3 Допустимый риск достигают с помощью итеративного процесса оценки риска и уменьшения риска
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node POSITION="top_or_left" ID="ID_1856472232" CREATED="1684435279860" MODIFIED="1684435279860"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    6 Достижение допустимого риска
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_1396733008" CREATED="1684435295423" MODIFIED="1684435295423"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    6.1 Для достижения уровня риска до допустимого применяют следующую процедуру
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_1651384019" CREATED="1684435305554" MODIFIED="1684435305554"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    а) определяют возможные группы пользователей продукции, процесса или услуги, включая типовую группу, группы со специальными потребностями, группу пожилых людей, а также любую возможную контактную группу (например, использование продукции, процесса или услуги детьми или контактов с ними детей);
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1967263299" CREATED="1684435318172" MODIFIED="1684435318172"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    б) определяют предназначенное использование и все виды возможного предсказуемого неправильного использования продукции, процесса или услуги;
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_292239849" CREATED="1684435325704" MODIFIED="1684435325704"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    в) выявляют каждую опасность (включающую в себя любую опасную ситуацию и вызывающее ущерб событие), возникающую на всех этапах и при всех условиях использования продукции, процесса или услуги, включая установку, эксплуатацию, ремонт и уничтожение/утилизацию;
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1589984820" CREATED="1684435332651" MODIFIED="1684435332651"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    г) оценивают риск (см. рисунок 1) для каждой определенной группы пользователей или контактирующей группы, возникающий вследствие определенной опасности;
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_97325399" CREATED="1684435338264" MODIFIED="1684435338264"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    д) принимают решение, является ли риск допустимым (например, по сравнению с подобной продукцией, процессами или услугами);
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1300437342" CREATED="1684435361801" MODIFIED="1684435361801"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    е) если риск является недопустимым, снижают уровень риска до допустимого.
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node ID="ID_110497611" CREATED="1684435371508" MODIFIED="1684435371508"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    6.2 Способы уменьшения риска следующие (в порядке приоритетов):
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_382662210" CREATED="1684435378719" MODIFIED="1684435378719"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    - разработка безопасного в своей основе проекта;
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1753952922" CREATED="1684435388290" MODIFIED="1684435388290"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    - защитные устройства и персональное защитное оборудование;
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1286399036" CREATED="1684435393984" MODIFIED="1684435393984"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    - информация по установке и применению;
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_89738585" CREATED="1684435398330" MODIFIED="1684435398330"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    - обучение;
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+<node POSITION="bottom_or_right" ID="ID_111234281" CREATED="1684435405960" MODIFIED="1684435405960"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    7 Включение аспектов безопасности в стандарты
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_1596753970" CREATED="1684435415988" MODIFIED="1684435415988"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    7.1 Типы стандартов на безопасность
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1733360045" CREATED="1684435433410" MODIFIED="1684435433410"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    7.2 Анализ предложений по разработке (пересмотру) стандартов
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_1305724113" CREATED="1684435470139" MODIFIED="1684435470139"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    а) Кому адресован стандарт?
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1941691972" CREATED="1684435475706" MODIFIED="1684435475706"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    б) Каким должен быть тип стандарта?
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_330414843" CREATED="1684435481314" MODIFIED="1684435481314"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    в) В чем состоит основная цель стандарта?
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_56570388" CREATED="1684435486835" MODIFIED="1684435486835"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    г) Как должен быть написан стандарт?
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node ID="ID_325515699" CREATED="1684435495744" MODIFIED="1684435495744"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    7.3 Подготовительная работа
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1888683186" CREATED="1684435505405" MODIFIED="1684435650263"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    7.4 Разработка проектов стандарто
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_1943544270" CREATED="1684435516725" MODIFIED="1684435516725"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    7.4.1 Общие положения
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_386283170" CREATED="1684435524666" MODIFIED="1684435524666"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    7.4.2 Информация для безопасного пользования
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_1634944695" CREATED="1684435532637" MODIFIED="1684435532637"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    7.4.2.1 Виды информации
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_911950364" CREATED="1684435539702" MODIFIED="1684435539702"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    7.4.2.2 Инструкции для пользования
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_507205078" CREATED="1684435548957" MODIFIED="1684435548957"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    7.4.2.3 Предупреждающие надписи
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node ID="ID_54742933" CREATED="1684435559496" MODIFIED="1684435559496"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    7.4.3 Упаковк
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1011775392" CREATED="1684435570509" MODIFIED="1684435570509"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    7.4.4 Безопасность при испытаниях
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
